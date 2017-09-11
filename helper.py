147ed4f229a23ca436f2bc529ad94323341fce3a
Charles Hong <csh6cw@virginia.edu>
2017-09-11 15:23:55 -0400

    Moved greeting() to a new module, helper.py

--- helper.py
+++ helper.py
@@ -1,10 +1,9 @@
 # Charles Hong (csh6cw)
 # 09/11/17
-# hello.py
+# helper.py
 __author__ = 'Charles Hong'
 __emailID__ = 'csh6cw'
 
-from helper import greeting
 
-if __name__ == '__main__':
-    greeting('hello')
+def greeting(msg):
+    print(msg)
