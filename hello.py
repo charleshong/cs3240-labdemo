f56731362111e8dcbe6e4a5c21d69292310f3310
Charles Hong <csh6cw@virginia.edu>
2017-09-11 15:42:50 -0400

    Added another interesting greeting, quack() in a new module, quack.py

--- hello.py
+++ hello.py
@@ -5,6 +5,8 @@ __author__ = 'Charles Hong'
 __emailID__ = 'csh6cw'
 
 from helper import greeting
+from quack import quack
 
 if __name__ == '__main__':
     greeting('hello')
+    quack('(quack)')
---
+++ quack.py
@@ -0,0 +1,9 @@
+# Charles Hong (csh6cw)
+# 09/11/17
+# quack.py
+__author__ = 'Charles Hong'
+__emailID__ = 'csh6cw'
+
+
+def quack(msg):
+    print(msg)
